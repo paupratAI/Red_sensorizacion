3afce8e389677cdc95ee4557ae420c5facc16023
paupratAI <pau.prat.moreno@estudiantat.upc.edu>
2024-04-11 21:37:09 +0200

    apartado b, falta comprobar que funciona bien

---
+++ main_b.py
@@ -0,0 +1,65 @@
+def parse_data(file_path):
+    # Diccionarios para almacenar los datos
+    intersections = []
+    paths = []
+    fixed = []
+    prohibited = []
+    path_flow = {}
+    intersections_paths = []
+    intersection_neighborhood = []
+    
+    # Leer el archivo
+    with open(file_path, 'r') as file:
+        content = file.read()
+        
+    # Dividir los contenidos por secciones
+    sections = content.split('\n\n')
+    for section in sections:
+        lines = section.split('\n')
+        header = lines[0].strip()
+        
+        if 'INTERSECTIONS' in header:
+            intersections = lines[1].split('\t')
+        elif 'PATHS' in header:
+            paths = lines[1].split('\t')
+        elif 'FIXED' in header:
+            fixed = lines[1].split()
+        elif 'PROHIBITED' in header:
+            prohibited = lines[1].split()
+        elif 'path_flow' in header:
+            for line in lines[1:]:
+                path_id, flow = line.split()
+                path_flow[path_id] = flow
+        elif 'path_intersections' in header:
+            for line in lines[1:]:
+                path_id, intersection_id = line.split()
+                intersections_paths.append(f'({intersection_id}, {path_id})')
+        elif 'intersection_neighborhood' in header:
+            for line in lines[1:]:
+                i1, i2 = line.split()
+                intersection_neighborhood.append(f'({i1}, {i2})')
+
+    return intersections, paths, fixed, prohibited, path_flow, intersections_paths, intersection_neighborhood
+
+
+def write_dat_file(intersections, paths, fixed, prohibited, path_flow, intersections_paths, intersection_neighborhood, dat_file_path):
+    with open(dat_file_path, 'w') as file:
+        file.write(f'set INTERSECTIONS := {" ".join(intersections)};\n')
+        file.write(f'set PATHS := {" ".join(paths)};\n')
+        file.write(f'set FIXED := {" ".join(fixed)};\n')
+        file.write(f'set PROHIBITED := {" ".join(prohibited)};\n')
+        file.write(f'set INTERSECTIONS_PATHS := {" ".join(intersections_paths)};\n') # está en formato (intersección, camino)
+        file.write(f'set NEIGHBORHOOD := {" ".join(intersection_neighborhood)};\n')
+        file.write('param pf :=\n')
+        for path_id, flow in path_flow.items():
+            file.write(f'  {path_id} {flow}\n')
+        file.write(';\n')
+
+input_file_path = 'datos.txt'
+output_dat_file_path = 'model.dat'
+
+# Procesar los datos
+data = parse_data(input_file_path)
+
+# Escribir el archivo .dat
+write_dat_file(*data, output_dat_file_path)
---
+++ model_b.mod
@@ -0,0 +1,33 @@
+# Conjuntos
+set INTERSECTIONS;
+set PATHS;
+set FIXED;
+set PROHIBITED;
+set INTERSECTIONS_PATHS within {INTERSECTIONS, PATHS}; # Conjunto de intersecciones y caminos que están conectados
+set NEIGHBORHOOD within {INTERSECTIONS, INTERSECTIONS};
+
+# Parámetros
+param pf {i in PATHS};  # pf_i representa el flujo en el camino i. Utilizar pf[i] para acceder al valor de pf_i
+
+# Variables
+var x {i in INTERSECTIONS} binary;
+var c {i in PATHS} binary;
+
+# Función objetivo
+maximize TotalFlow: sum {i in PATHS} c[i] * pf[i];
+
+# Restricciones
+subject to SensorCoverage {i in PATHS}:
+    sum {(j,i) in INTERSECTIONS_PATHS} x[j] >= 2 * c[i];
+
+subject to MaxSensors:
+    sum {j in INTERSECTIONS} x[j] <= 15;
+
+subject to SensorsFixed{j in FIXED}:
+    x[j] = 1;
+
+subject to SensorsProhibited{j in PROHIBITED}:
+    x[j] = 0;
+
+subject to NeighborhoodRestriction {(i, j) in NEIGHBORHOOD}:
+    x[i] + x[j] <= 1;
